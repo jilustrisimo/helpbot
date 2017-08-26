9fcda68a52eb5dce01da413c98007cadf8ed1c94
github username <jilustrisimo@users.noreply.github.com>
2017-08-26 12:25:16 -0400

    built study_groups resp

--- slack-helpbot/study_groups.rb
+++ slack-helpbot/study_groups.rb
@@ -1,5 +1,21 @@
 module SlackHelpBot
   Study_groups = <<-Study_groups.freeze
-    Study Groups
+    *Study Groups*
+
+   >You can find everything you need to know about study-groups here: http://help.learn.co/study-groups
+
+    There are multiple types of staff lead study groups.
+    They are categorized as:
+
+    • *Lab Walkthrough:* Reviews of labs throughout the curriculum
+
+    • *Portfolio Project Prep:* A chance to get a head start or some help with your portfolio project for the section listed for the group
+
+    • *Open Office Hours (OOH):* A time to ask any questions you have regarding the topics listed for the group
+
+    • *Live Lectures:* Groups conducted by Section Leads that cover concepts throughout the curriculum
+
+    • *Community:* Social gatherings.  A time to talk about your trial and tribulations and everything in between.
+
   Study_groups
 end
