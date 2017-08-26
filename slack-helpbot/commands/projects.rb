5429e850d9043a4d0f7f504ebf9db0d554944ec9
github username <jilustrisimo@users.noreply.github.com>
2017-08-26 00:27:09 -0400

    prepping study_groups

--- slack-helpbot/commands/projects.rb
+++ slack-helpbot/commands/projects.rb
@@ -3,7 +3,7 @@ module SlackHelpBot
     class Projects < SlackRubyBot::Commands::Base
       command 'group projects'
       match(/pair/i)
-      scan(/team.up/i)
+      scan(/team\sup/i)
 
       def self.call(client, data, _match)
         client.say(channel: data.channel, text: ["Hey there <@#{data.user}>!", PROJECTS])
--- slack-helpbot/commands/study_groups.rb
+++ slack-helpbot/commands/study_groups.rb
@@ -1,10 +1,10 @@
 module SlackHelpBot
   module Commands
     class Projects < SlackRubyBot::Commands::Base
-      scan(/study.group/i)
+      scan(/study\sgroup/i)
 
       def self.call(client, data, _match)
-        client.say(channel: data.channel, text: ["Hey there <@#{data.user}>!"])
+        client.say(channel: data.channel, text: "Hey <@#{data.user}>!  You can find everything you need to know about study-groups here: http://help.learn.co/study-groups")
       end
     end
   end
