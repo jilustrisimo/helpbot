2abfd6228403e13f9090da39b69d2b4677935bcf
github username <jilustrisimo@users.noreply.github.com>
2017-08-29 00:03:14 -0400

    Changed output for projects

--- slack-helpbot/projects.rb
+++ slack-helpbot/projects.rb
@@ -1,17 +1,24 @@
 module SlackHelpBot
   PROJECTS = <<-PROJECTS.freeze
-    *Teaming up for Group Projects*
+    *Group Projects*
 
-    _NOTE: you *must* work on portfolio projects on your own_
+    Group Projects can be found at the end of the following sections:
 
-    Collaborative programming in the program is an amazing way to get used to working with a team!
+    • OO Ruby
+    • Sinatra
+    • Rails
+    • Rails/Jquery
+
+    They are a great way to help solidfy the knowsledge youve gained from the section, as well as give you a chance to work with people as a team on an app!
 
     You can join the following channel and check if anyone else has been looking for a team for the same project as you.  Likewise, you can post that you are looking team up in the channel as well, just make sure you specify the project!
-
+    
     <#C6U9YT99T>
-
-    Once you have your team you can learn more about how to get going here:
-
+    
+    Learn more about how to get going here:
+    
     http://help.learn.co/your-learn-account/learn-lights/group-projects
+
+    _NOTE: you *must* work on portfolio projects on your own_
   PROJECTS
 end
