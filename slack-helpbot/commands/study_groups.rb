f9dfd98be94b3e28ed56fec2e9ac86888cf3e664
github username <jilustrisimo@users.noreply.github.com>
2017-08-29 00:03:37 -0400

    added text to resp for study_groups

--- slack-helpbot/commands/study_groups.rb
+++ slack-helpbot/commands/study_groups.rb
@@ -2,7 +2,7 @@ module SlackHelpBot
   module Commands
     class Projects < SlackRubyBot::Commands::Base
       command 'study groups' do |client, data, _match|
-        client.say(channel: data.channel, text: ["Hey <@#{data.user}>!", Study_groups])
+        client.say(channel: data.channel, text: ["Hey <@#{data.user}>! Here's what I have on study groups:", Study_groups])
       end
     end
   end
