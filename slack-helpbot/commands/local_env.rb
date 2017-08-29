7c7d464f2d53f8686fee6bee26d4ed4ecb799dff
github username <jilustrisimo@users.noreply.github.com>
2017-08-29 00:15:15 -0400

    change contstant

--- slack-helpbot/commands/local_env.rb
+++ slack-helpbot/commands/local_env.rb
@@ -2,7 +2,7 @@ module SlackHelpBot
   module Commands
     class LocalENV < SlackRubyBot::Commands::Base
       command 'local environment' do |client, data, _match|
-        client.say(channel: data.channel, text: ["Welcome to the brave new world <@#{data.user}>!", LOCAL_ENV])
+        client.say(channel: data.channel, text: ["Welcome to the brave new world <@#{data.user}>!", LOCAL])
       end
     end
   end
