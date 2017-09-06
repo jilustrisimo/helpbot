d2bb2bce9d96f74620e670f047b1bf521beea7ba
github username <jilustrisimo@users.noreply.github.com>
2017-09-06 19:26:42 -0400

    possible fix for loop

--- slack-helpbot/commands/projects.rb
+++ slack-helpbot/commands/projects.rb
@@ -1,8 +1,11 @@
 module SlackHelpBot
   module Commands
     class Projects < SlackRubyBot::Commands::Base
-      command 'group projects' do |client, data, _match|
-        client.say(channel: data.channel, text: ["Hey there <@#{data.user}>!", PROJECTS])
+      command 'group'
+      scan(/^helpbot\b.*?\bgroup.project\w?\b.*?$/mi)
+
+      def self.call(client, data, _match)
+        client.say(channel: data.channel, text: ["Hey there <@#{data.user}>!", PROJECTS]) unless data.user.include? 'SLACKBOT'
       end
     end
   end
--- slack-helpbot/projects.rb
+++ slack-helpbot/projects.rb
@@ -9,14 +9,14 @@ module SlackHelpBot
     • Rails
     • Rails/Jquery
 
-    They are a great way to help solidfy the knowsledge youve gained from the section, as well as give you a chance to work with people as a team on an app!
+    They are a great way to help solidfy the knowledge youve gained from the section, as well as give you a chance to work with people as a team on an app!
 
-    You can join the following channel and check if anyone else has been looking for a team for the same project as you.  Likewise, you can post that you are looking team up in the channel as well, just make sure you specify the project!
-    
+    You can join the following channel and check if anyone else has been looking for a team for the same project as you.  Likewise, you can post that you are hoping to team up in the channel as well, just make sure you specify the project!
+
     <#C6U9YT99T>
-    
+
     Learn more about how to get going here:
-    
+
     http://help.learn.co/your-learn-account/learn-lights/group-projects
 
     _NOTE: you *must* work on portfolio projects on your own_
